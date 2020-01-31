37e6d3c9a82d699e55efafd55eff01ac39a97a37
League Student <league@iMac-15.attlocal.net>
2020-01-30 19:59:22 -0800

    gridworld

---
+++ src/_06_gridworld/Gridworld.java
@@ -0,0 +1,13 @@
+package _06_gridworld;
+
+import info.gridworld.world.World;
+
+public class Gridworld {
+	
+ 	
+	public static void main(String[] args) {
+		World juice = new World();
+juice.show();
+
+}
+}
